4eadb0896e1e26fb3b9f0aef73066c9e30989f29
Kazi Sadman Ahmed <sadman.bd@gmail.com>
2018-04-18 20:28:05 +0600

    Simplify

--- wifi-hotspot-autoconnect/wifi_hotspot_autoconnect.py
+++ wifi-hotspot-autoconnect/wifi_hotspot_autoconnect.py
@@ -2,38 +2,21 @@ import click
 import library.hotspot_wrapper as wrapper
 
 
-def hotspot_exists():
-    return wrapper.hotspot_exists()
-
-
-def autoconnect_status():
-    return wrapper.status_hotspot_autoconnect()
-
-
-def autoconnect_set(value: bool):
-    wrapper.set_autoconnect(value)
-
-
 CONTEXT_SETTINGS = dict(help_option_names=[' ', '--help', '-h'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option("--status", help="Shows Autoconnection Status", is_flag=True,
-              default=None)
-@click.option("--set", help="Set Hotspot autoconnection to True/False",
-              type=bool)
-def main(status, set):
+@click.option("--status", help="Shows Autoconnection Status", is_flag=True, default=None)
+@click.option("--on/--off", help="Set Hotspot autoconnection to On/Off", default=None)
+def main(status, on):
     """wifi_hotspot_autoconnect helps enable/disable hotspot
     autoconnect feature """
-    if(status):
-        click.echo("Hotspot Auto connection status is: " +
-                   str(autoconnect_status()))
-        return
-    if isinstance(set, bool):
-        wrapper.set_autoconnect(set)
-    if status is None and set is None:
+    if status:
+        click.echo("Hotspot Auto connection status is: " + str(wrapper.status_hotspot_autoconnect()))
+    elif on != None:
+        wrapper.set_autoconnect(on)
+    else:
         print("No configuration detected. Please use --help for instructions")
-    # autoconnect_set(set)
 
 
 if __name__ == '__main__':
