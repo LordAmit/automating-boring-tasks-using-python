f513c9e73251781516c7076461f6dd605d7c2c3b
Amit Seal Ami <lordamit@gmail.com>
2018-07-09 03:10:39 +0600

    added functionalities

---
+++ scanned_image_beautifier/tests/test_filehandler.py
@@ -0,0 +1,39 @@
+import unittest
+import library.file_handler as file_handler
+
+file_address = "/home/username/file.bmp"
+file_address_with_modified = "/home/username/modified_file.bmp"
+filename = "file"
+ext = ".bmp"
+fake_ext = ".txt"
+file_dir = "/home/username/"
+
+
+class Test_filehandler(unittest.TestCase):
+    def test_filename_wo_ext(self):
+        global file_address
+        global filename
+        self.assertEqual(file_handler.extract_filename_wo_ext(
+            file_address), filename)
+
+    def test_filepath_modified_prefix(self):
+        global file
+        global file_address
+        global file_address_with_modified
+        self.assertEqual(file_handler.filepath_modified_suffix(
+            file_address), file_address_with_modified)
+
+    def test_extract_dir(self):
+        global file_address
+        global file_dir
+        self.assertEqual(file_handler.extract_dir(file_address), file_dir)
+
+    def test_extract_ext(self):
+        global file_address
+        global ext
+        self.assertEqual(file_handler.extract_ext(file_address), ext)
+
+    def test_check_ext(self):
+        global ext
+        self.assertTrue(file_handler.check_ext(ext))
+        self.assertFalse(file_handler.check_ext("habijabi"))
---
+++ scanned_image_beautifier/tests/test_image_handler.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from library import image_handler as img
+
+
+image_file_address = "/home/amit/git/automating-boring-tasks-using-python/scanned_image_beautifier/test.jpg"
+thumbnail_file_address = \
+    "/home/amit/git/automating-boring-tasks-using-python/scanned_image_beautifier/test_thumbnail.jpg"
+
+class TestImageHandler(TestCase):
+
+    pass
+
