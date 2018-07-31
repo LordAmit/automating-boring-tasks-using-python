393a6710b86c9bd68fb2c3c78514f87b49057ccd
Amit Seal Ami <lordamit@gmail.com>
2018-07-31 18:41:05 +0600

    added pdf options

---
+++ scanned_image_beautifier/gui.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui.ui'
+#
+# Created by: PyQt4 UI code generator 4.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
