b08f1e94a4bce2ce36af57ebb2a81e6488f8be00
Amit Seal Ami <lordamit@gmail.com>
2019-08-04 15:25:18 -0400

    added option for file walker

--- image_in_window_screensaver/file_walker.py
+++ image_in_window_screensaver/file_walker.py
@@ -1,16 +1,20 @@
 from os import path as Path
 from typing import List
 import custom_log as l
-import sys, os
+import os
+import argument_handler as argh
 
 
-def walk(path: str) -> List:
+def walk() -> List:
     """
     walks through a path to return list of absolute path of images with png or jpg extensions
     :param path: to the directory where images will be searched for
     :return: List of string path of images
     """
     image_paths = []
+    path: str = argh.get_path()
+    l.log(path)
+    ignore: str = argh.get_ignore_word()
     l.log("checking path integrity")
     if Path.exists(path):
         l.log([path, "exists"])
@@ -26,5 +30,25 @@ def walk(path: str) -> List:
                 file_path = os.path.join(folderName, filename)
 
                 l.log([file_path, filename])
+                if ignore and is_exclude_image(file_path, ignore):
+                    continue
                 image_paths.append(file_path)
     return image_paths
+
+
+def is_exclude_image(image_path: str, ignore_word: str) -> str:
+    import custom_log as l
+    l.log(ignore_word)
+    # exit()
+    if ',' in ignore_word:
+        list_ignores = ignore_word.split(',')
+
+        for word in list_ignores:
+            if image_path.lower().find(word) is not -1:
+                l.log(["found entry in list_ignore: ", image_path, word])
+                return True
+    else:
+        if image_path.lower().find(ignore_word) is not -1:
+            l.log(["found entry in list_ignore: ", image_path, ignore_word])
+            return True
+    return False
