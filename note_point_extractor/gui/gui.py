267cbed713c46e0abd3e52cb814cd55919340c6e
Amit <lordamit@gmail.com>
2019-12-26 10:43:25 -0800

    pyside2 ui to code

--- note_point_extractor/gui/gui.py
+++ note_point_extractor/gui/gui.py
@@ -1,60 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'gui.ui'
+# Form implementation generated from reading ui file 'gui.ui',
+# licensing of 'gui.ui' applies.
 #
-# Created by: PyQt4 UI code generator 4.12.1
+# Created: Thu Dec 26 10:37:54 2019
+#      by: pyside2-uic  running on PySide2 5.13.1
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PySide2 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMaximumSize(QtCore.QSize(800, 600))
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.gridLayout_2 = QtGui.QGridLayout(self.centralwidget)
-        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
-        self.gridLayout = QtGui.QGridLayout()
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.textEdit_input = QtGui.QTextEdit(self.centralwidget)
-        self.textEdit_input.setObjectName(_fromUtf8("textEdit_input"))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.textEdit_input = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_input.setObjectName("textEdit_input")
         self.gridLayout.addWidget(self.textEdit_input, 1, 0, 1, 1)
-        self.textedit_output = QtGui.QTextEdit(self.centralwidget)
-        self.textedit_output.setObjectName(_fromUtf8("textedit_output"))
+        self.textedit_output = QtWidgets.QTextEdit(self.centralwidget)
+        self.textedit_output.setObjectName("textedit_output")
         self.gridLayout.addWidget(self.textedit_output, 1, 1, 1, 1)
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setObjectName(_fromUtf8("label"))
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
         self.gridLayout.addWidget(self.label_2, 0, 1, 1, 1)
-        self.button_output = QtGui.QPushButton(self.centralwidget)
-        self.button_output.setObjectName(_fromUtf8("button_output"))
+        self.button_output = QtWidgets.QPushButton(self.centralwidget)
+        self.button_output.setObjectName("button_output")
         self.gridLayout.addWidget(self.button_output, 2, 0, 1, 1)
-        self.button_markdown_output = QtGui.QPushButton(self.centralwidget)
-        self.button_markdown_output.setObjectName(_fromUtf8("button_markdown_output"))
+        self.button_markdown_output = QtWidgets.QPushButton(self.centralwidget)
+        self.button_markdown_output.setObjectName("button_markdown_output")
         self.gridLayout.addWidget(self.button_markdown_output, 2, 1, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -63,9 +51,9 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Notes Point Extractor", None))
-        self.label.setText(_translate("MainWindow", "Input Text", None))
-        self.label_2.setText(_translate("MainWindow", "Output Text", None))
-        self.button_output.setText(_translate("MainWindow", "Output", None))
-        self.button_markdown_output.setText(_translate("MainWindow", "Markdown Output", None))
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "Notes Point Extractor", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("MainWindow", "Input Text", None, -1))
+        self.label_2.setText(QtWidgets.QApplication.translate("MainWindow", "Output Text", None, -1))
+        self.button_output.setText(QtWidgets.QApplication.translate("MainWindow", "Output", None, -1))
+        self.button_markdown_output.setText(QtWidgets.QApplication.translate("MainWindow", "Markdown Output", None, -1))
 
