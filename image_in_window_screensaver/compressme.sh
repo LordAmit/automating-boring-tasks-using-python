10ad2317d0e5029662444d37da2ad51d31d526bd
Amit <lordamit@gmail.com>
2021-12-15 11:29:37 +0600

    script for compressor

---
+++ image_in_window_screensaver/compressme.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Users/amitseal/git/automating-boring-tasks-using-python/image_in_window_screensaver/venv/bin/python /Users/amitseal/git/automating-boring-tasks-using-python/image_in_window_screensaver/compress_images.py --path /Users/amitseal/Pictures/x/portrait/ --backup /Users/amitseal/Pictures/backup/
+/Users/amitseal/git/automating-boring-tasks-using-python/image_in_window_screensaver/venv/bin/python /Users/amitseal/git/automating-boring-tasks-using-python/image_in_window_screensaver/compress_images.py --path /Users/amitseal/Pictures/x/landscape/ --backup /Users/amitseal/Pictures/backup/
