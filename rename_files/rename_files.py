48c8741535421d185894fec5841a0b0ca96fc742
Amit <lordamit@gmail.com>
2022-05-04 12:19:57 -0400

    details

--- rename_files/rename_files.py
+++ rename_files/rename_files.py
@@ -10,17 +10,16 @@ def rename_files(prefix: str, force_run: bool = False):
     files = file_walker.get_files()
     for i in range(0, len(files)):
         current_filepath = files[i]
-        # current_filename = os.path.basename(current_filepath)
+
         current_path, current_filename = os.path.split(current_filepath)
         old_prefix, extension = os.path.splitext(current_filename)
-        # l.log([current_path, current_filename, old_prefix, extension])
+
         new_filepath = current_path + '/'+prefix + str(i) + extension
-        # print("{0} --> {1}".format('a', 'b'))
+        print("dry run, no changes will be made")
         print("{0} --> {1}".format(current_filepath, new_filepath))
         if force_run:
+            print("force run, changes will be made")
             os.rename(current_filepath, new_filepath)
-        # new_filename = prefix
-        # break
 
 
 if __name__ == '__main__':
