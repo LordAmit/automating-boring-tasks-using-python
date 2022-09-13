2c91b4c6324cf572197326ad3c0522fb89a3494a
Amit <lordamit@gmail.com>
2022-09-13 17:13:40 -0400

    add_comment_page.sh

---
+++ pdfs/add_comment_page.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Users/amitseal/git/automating-boring-tasks-using-python/pdfs/venv/bin/python add_comment_page.py $1 /Users/amitseal/git/automating-boring-tasks-using-python/pdfs/template/cornelllined.pdf
