4d3b88f281757a0a937f74161efb692dbeb89840
Amit <lordamit@gmail.com>
2022-09-20 02:37:55 -0400

    fixed pdf insert bug

--- pdfs/add_comment_page.py
+++ pdfs/add_comment_page.py
@@ -1,26 +1,30 @@
 from sys import argv
-from PyPDF2 import PdfFileReader, PdfFileWriter, PdfWriter
+from PyPDF2 import PdfMerger, PdfFileReader
 from pip import main
 import sys
 
-def get_pages(input_pdf: PdfFileReader):
-    return input_pdf.getNumPages()
 
+def add_pages(input_pdf_path: str, template_pdf_path: str):
+    input_pdf = open(input_pdf_path, "rb")
+    page_count: int = PdfFileReader(input_pdf_path).getNumPages()
+    template = open(template_pdf_path, "rb")
+    merger = PdfMerger()
 
-def add_pages(input_pdf_path: str, template_pdf_path: str) -> PdfFileWriter:
-    writer = PdfFileWriter()
-    input_reader = PdfFileReader(input_pdf_path)
-    template_reader = PdfFileReader(template_pdf_path)
+    print("original page count: " + str(page_count))
+    for i in range(0, page_count):
+        print("adding templatr page at "+ str(2 * i + 1))
+        merger.append(fileobj=input_pdf, pages=(i, i+1))
+        append_index = (2 * i) + 1
+        merger.merge(position=append_index, fileobj=template, pages=(0, 1))
 
-    for i in range(0, get_pages(input_reader)):
-        writer.addPage(input_reader.getPage(i))
-        writer.addPage(template_reader.getPage(0))
+    template.close()
+    input_pdf.close()
+    write_file(input_pdf_path, merger)
 
-    return writer
-
-def write_file(input_pdf_path: str, writer: PdfFileWriter):
+def write_file(input_pdf_path: str, merger: PdfMerger):
     with open(input_pdf_path+"merged.pdf", "wb") as out:
-        writer.write(out)
+        merger.write(out)
+    merger.close()
 
 def main():
 
@@ -31,7 +35,7 @@ def main():
         exit()
     input_pdf_path = sys.argv[1]
     template_pdf_path = sys.argv[2]
-    write_file(input_pdf_path, add_pages(input_pdf_path, template_pdf_path))
+    add_pages(input_pdf_path, template_pdf_path)
 
 if __name__ == "__main__":
     main()
